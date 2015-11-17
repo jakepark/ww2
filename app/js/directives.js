287a620cf991d9e3c8b84a5cea50086156a48529
jake park <jake.park@caa.columbia.edu>
2015-11-16 20:26:39 -0500

    Custom directive, paragraph or image display

--- app/js/directives.js
+++ app/js/directives.js
@@ -7,8 +7,30 @@ ww2Controllers.directive('paraImg', [function (){
     return {
       restrict: 'E',
       link: function(scope, element, attrs){
-        var tag='<h6>Temp</h6>';
-        element.append(tag);
+
+        var page = scope.page;
+        var key = scope.key;
+        var val = scope.value;
+        var tag_open;
+        var tag_close;
+
+        var content = scope.chapter.pages[page][key][val];
+
+        var item;
+
+
+        if (key === "paragraphs"){
+          tag_open = "<p>"
+          tag_close = "</p>"
+
+        } else if (key === "images"){
+          tag_open = "<img src="
+          tag_close = ">"
+        }
+
+        item = tag_open + content + tag_close
+        
+        element.append(item);
 
       }
     }
