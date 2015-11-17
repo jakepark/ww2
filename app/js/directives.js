fdf6a94eedee9190a3ca4d34ae0d7325fa828006
jake park <jake.park@caa.columbia.edu>
2015-11-16 20:09:39 -0500

    custom directive for paragraph or image element

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,16 @@
 'use strict';
 
 /* Directives */
+var ww2Controllers = angular.module('ww2Controllers');
+
+ww2Controllers.directive('paraImg', [function (){
+    return {
+      restrict: 'E',
+      link: function(scope, element, attrs){
+        var tag='<h6>Temp</h6>';
+        element.append(tag);
+
+      }
+    }
+
+}]);
