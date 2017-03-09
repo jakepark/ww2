345cea76c0133238ddcb661f2effc2788f97d412
jake park <jake.park@caa.columbia.edu>
2017-03-09 01:12:11 -0500

    fix transclude error message

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,42 +3,6 @@
 /* Directives */
 var ww2Controllers = angular.module('ww2Controllers');
 
-ww2Controllers.directive('paraImg', [function (){
-    return {
-      restrict: 'E',
-      link: function(scope, element, attrs){
-
-        var page = scope.page;
-        var key = scope.key;
-        var val = scope.value;
-        var tag_open;
-        var tag_close;
-
-        var content = scope.chapter.pages[page][key][val];
-
-        var item;
-
-
-        if (key === "paragraphs"){
-          tag_open = "<p>"
-          tag_close = "</p>"
-
-        } else if (key === "images"){
-
-
-          tag_open = "<img src="
-          tag_close = ">"
-        }
-
-        item = tag_open + content + tag_close
-
-        element.append(item);
-
-      }
-    }
-
-}]);
-
 ww2Controllers.directive('modalDialog', function() {
   return {
     restrict: 'E',
